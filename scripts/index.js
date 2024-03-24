09a2cc8e466d42854d804a6b58f53d1dbcde6bf8
Ethan Rudy <16.erudy@gmail.com>
2024-03-24 01:24:55 -0700

    Documentation Updates
    Comments!

--- scripts/index.js
+++ scripts/index.js
@@ -6,38 +6,62 @@ API KEY
 */
 
 /**
- * TODO
+ * OKAY READ THIS
  * 
- * Adjust the error in grid coordinates
- * Unordered graph and list pathfinding
+ * These two js files interact really oddly.
+ * They can sometimes talk to eachother via functions, but not access eachothers
+ * global vars. Because that totally makes sense. Fuck JS
  * 
- * Mockup:
- *  - Scheduling
- *  - Weather API
+ * The general rule of thumb I, Ethan Rudy, have followed is, this file manages
+ * HTML events, the map API, and any non-grid interactivity
+ * sketch.js on the otherhand handles all canvas events & visuals, pathing, etc.
  */
 
-var mapObject;          // Map object
+/**
+ * TODO:
+ * 
+ * NEEDED:
+ *  Adjust grid to canvas lat lng error
+ * 
+ * Optional:
+ *  Optimize Pathfinding
+ */
+
+var mapObject;    // Map API object
 var rectangle;    // Rectangle Object
 var infowindow;   // Info window
 
 var marker1;      // North West Marker
 var marker2;      // South East Marker
 
-var rectangleLat = [];
-var rectangleLng = [];
+var mapOptions;   // Maps API option tags
 
-// Map options
-var mapOptions;   // Maps API tags
-
-
+/**
+ * Initialize Map
+ * 
+ * Is called when the website loads
+ * 
+ * The follwing happens:
+ * - Initializes the map object with som default parameters (mostly for testing and will be changed later)
+ * - It places two interactive markers, a NW and SE
+ * - Draws the translucent rectangle visualizing the geo-fence
+ */
 function initMap() {
 
+  // Create the info window
   var infowindow = new google.maps.InfoWindow();
 
+  // Default map options
   mapOptions = {
-    zoom: 16,
+    zoom: 16,   
+    // !READ ME! Zoom is stupid, TLDR: 16 is twice as zoomed as 15, and 17 is twice is zoomed as 16. Zoom Level = (x - 1) * 2 == x == (x + 1) / 2
+    // It is an INTEGER, meaning theres no middle ground, everytime you zoom in or out, you lose/gain 2x the detail, not very customizable i know :(
+
     center: new google.maps.LatLng(33.434625, -111.939896),
+    // Tempe default value
+
     mapTypeId: 'satellite'
+    // Default view mode, others are still enabled
   };
 
   // Create map with given options
@@ -50,26 +74,26 @@ function initMap() {
     draggable: true,
     title: 'marker1'
   });
-  // Add the 'click for info' listener
+  // Add the 'click for info' listener to marker 1
   google.maps.event.addListener(marker1, 'click', function (evt) {
     infowindow.setContent("Top Left: " + marker1.getPosition().toUrlValue(6));
     infowindow.open(mapObject, this);
   });
 
-  // Initialize Marker 2
+  // Initialize Marker 2 
   marker2 = new google.maps.Marker({
     position: new google.maps.LatLng(33.433589, -111.932178),
     map: mapObject,
     draggable: true,
     title: 'marker2'
   });
-  // Add the 'click for info' listener
+  // Add the 'click for info' listener to marker 2
   google.maps.event.addListener(marker2, 'click', function (evt) {
     infowindow.setContent("Bottom Right: " + marker2.getPosition().toUrlValue(6));
     infowindow.open(mapObject, this);
   });
 
-  // Initialize Rectangle visuals
+  // Initialize Rectangle visuals (Translucent Red)
   rectangle = new google.maps.Rectangle({
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
@@ -82,7 +106,7 @@ function initMap() {
       marker2.getPosition())
   });
 
-  // Calculate side lengths
+  // Calculate approximate side lengths in arc seconds
   var leftSideDist = Math.round((marker2.getPosition().lng() - marker1.getPosition().lng()) * 10000) / 100;
   var belowSideDist = Math.round((marker2.getPosition().lat() - marker1.getPosition().lat()) * 10000) / 100;
 
@@ -90,124 +114,100 @@ function initMap() {
   google.maps.event.addListener(marker1, 'dragend', function () {
     rectangle.setBounds(new google.maps.LatLngBounds(marker1.getPosition(), marker2.getPosition()));
     leftSideDist = Math.round((marker2.getPosition().lng() - marker1.getPosition().lng()) * 10000) / 100;
-    clearGrid();
   });
   google.maps.event.addListener(marker2, 'dragend', function () {
     rectangle.setBounds(new google.maps.LatLngBounds(marker1.getPosition(), marker2.getPosition()));
     belowSideDist = Math.round((marker2.getPosition().lat() - marker1.getPosition().lat()) * 10000) / 100;
-    clearGrid();
   });
 
-  // Make the first initial grid
-  //makeGrid();
-  var genButton = document.getElementById("grid_button");
+
+
+
+  // HTML Button Listeners
+
+  // Generate Grid Button
+  let genButton = document.getElementById("grid_button");
   genButton.addEventListener("click", makeGrid);
-  var enButton = document.getElementById("enable_button");
-  var diButton = document.getElementById("disable_button");
+
+  // Enable and Disable Cell(s) Button
+  let enButton = document.getElementById("enable_button");
+  let diButton = document.getElementById("disable_button");
   enButton.addEventListener("click", setEnabled);
   diButton.addEventListener("click", setDisabled);
-  var pathButton = document.getElementById("generate_path")
+
+  // Generate Path Button
+  let pathButton = document.getElementById("generate_path")
   pathButton.addEventListener("click", generatePath);
 }
 
+/**
+ * Make Grid (Map API)
+ * 
+ * Creates a grid on the
+ */
 function makeGrid() {
-  // Reset for the new grid
-  // Needed to clear out the old grid
-  clearGrid();
 
-  var m1Lat = marker1.getPosition().lat();
-  var m1Lng = marker1.getPosition().lng();
-  var m2Lat = marker2.getPosition().lat();
-  var m2Lng = marker2.getPosition().lng();
+  // Grabbing positions to make life easier & code readable
+  let m1Lat = marker1.getPosition().lat();
+  let m1Lng = marker1.getPosition().lng();
+  let m2Lat = marker2.getPosition().lat();
+  let m2Lng = marker2.getPosition().lng();
   
-  var leftSideDist = m2Lng - m1Lng;
-  var belowSideDist = m2Lat - m1Lat;
+  // Calculate side lengths
+  let leftSideDist = m2Lng - m1Lng;
+  let belowSideDist = m2Lat - m1Lat;
 
   
 
-  // CHANGLE ALL OF THIS TO AUTO CALCULATE ~DONE~
-  // https://stackoverflow.com/questions/3024404/transform-longitude-latitude-into-meters
-  var circumAtLat = 40075160 * Math.cos(toRadians(m1Lat));
-  var dividerLat = Math.abs(belowSideDist) * circumAtLat / 360;
-  var dividerLng = Math.abs(leftSideDist) * 40008000 / 360;
+  // Calculate the correct dimensions based of longitude and latitude
+  let circumAtLat = 40075160 * Math.cos(toRadians(m1Lat));
+  let dividerLat = Math.abs(belowSideDist) * circumAtLat / 360;
+  let dividerLng = Math.abs(leftSideDist) * 40008000 / 360;
   dividerLat = Math.round(dividerLat / 5); // 5 Meters
   dividerLng = Math.round(dividerLng / 5); // 5 Meters
 
-  var excLat = belowSideDist / dividerLat;
-  var excLng = leftSideDist / dividerLng;
-
   // Add data to the info panel
-  //document.getElementById('info').innerHTML += "dividerLat=" + dividerLat + ", excLat=" + excLat + "<br>";
-  //document.getElementById('info').innerHTML += "dividerLng=" + dividerLat + ", excLng=" + excLng + "<br>";
-  //document.getElementById('info').innerHTML += "m1=" + marker1.getPosition().toUrlValue(6) + "<br>";
-  //document.getElementById('info').innerHTML += "m2=" + marker2.getPosition().toUrlValue(6) + "<br>";
-
-  // Draw Grid
-  for (var i = 0; i < dividerLat; i++) {
-    if (rectangleLng[i] == null) rectangleLng[i] = [];
-
-    for (var j = 0; j < dividerLng; j++) {
-
-      if (rectangleLng[i][j] = null) rectangleLng[i][j] = {};
+  // Uncomment to get them back lol
+  /**
+   * let excLat = belowSideDist / dividerLat;
+   * let excLng = leftSideDist / dividerLng;
+   * document.getElementById('info').innerHTML += "dividerLat=" + dividerLat + ", excLat=" + excLat + "<br>";
+   * document.getElementById('info').innerHTML += "dividerLng=" + dividerLat + ", excLng=" + excLng + "<br>";
+   * document.getElementById('info').innerHTML += "m1=" + marker1.getPosition().toUrlValue(6) + "<br>";
+   * document.getElementById('info').innerHTML += "m2=" + marker2.getPosition().toUrlValue(6) + "<br>";
+   */
 
 
-      rectangleLng[i][j] = new google.maps.Rectangle({
-        strokeColor: '#FFFFFF',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.1,
-        map: mapObject,
-        bounds: new google.maps.LatLngBounds(
-          new google.maps.LatLng(m1Lat + (excLat * i), m1Lng + (excLng * j)),
-          new google.maps.LatLng(m1Lat + (excLat * (i + 1)), m1Lng + (excLng * (j + 1))))
+  /**
+   * We used to draw the grid on the maps API object, but I thought it was confusing
+   * It was here, obv not anymore
+   */
 
-      });
-      //document.getElementById('info').innerHTML += "[i=" + i + ",j=" + j + "]:" + rectangleLng[i][j].getBounds() + "<br>";
-
-
-
-    } // Lat Loop ends
-
-  } // Lng Loop ends
-
-
+  // Set distance
   document.getElementById('left').value = leftSideDist;
   document.getElementById('bottom').value = belowSideDist;
 
+  // Find the meters per pixel ratio
   let meters_per_pixel = 156543.03392 * Math.cos(mapObject.getCenter().lat() * Math.PI / 180) / Math.pow(2, mapObject.getZoom());
   
   // Canvas is 600 / 600, need amount of 5 m grid cells that fit 
   let canvasGridDim = 600 * meters_per_pixel / 5;
   let pixelsPerSquare = 640.0 / canvasGridDim;
 
+  // Send the data to sketch.js
   updateGridData(canvasGridDim, pixelsPerSquare);
 
+  // Convert the maps API data to a static maps API image, yes its two difference services (also to the p5js canvas)
   toImage();
 }
 
 /**
- * Clear Grid
- * 
- * Clears the current grid rectangles
- */
-function clearGrid(){
-  for (var x in rectangleLng) {
-    for (var y in rectangleLng[x]) {
-      if (rectangleLng[x][y] != null && rectangleLng[x][y].setMap) {
-        rectangleLng[x][y].setMap(null)
-        rectangleLng[x][y] = null;
-      }
-    }
-  }
-}
-
-
-/**
  * To Image
  * 
- * Exports the current map view and data 
- * and pipes it into an image tag
+ * Exports the current map view data to the google "static maps" API. 
+ * and pipes it to sketch.js
+ * 
+ * TLDR: Take the current view, and make it an image, and put it in on the canvas
  */
 function toImage() {
 
@@ -218,6 +218,10 @@ function toImage() {
   // Find the center of the selected area
   var latCenter = (nW.lat() + sE.lat()) / 2;
   var lngCenter = (nW.lng() + sE.lng()) / 2;
+
+
+  // API Request creation
+
   // Starter URL
   var staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap";
 
@@ -236,12 +240,13 @@ function toImage() {
   // Set the API key value
   staticMapUrl += "&key=" + "AIzaSyBMXidTHFP4iLQDGxo34ODeXp7dMn6869Q";
 
-  // Set the background image
+  // Set the background image (pipe to sketh.js)
   updateBackground(staticMapUrl);
 }
 
 /**
  * To Radians
+ * Degrees to radians, Duh
  * 
  * @param degrees Angle measurement in degrees
  * 
@@ -251,10 +256,5 @@ function toRadians(degrees){
   return degrees * Math.PI / 180;
 }
 
+// Load function
 window.initMap = initMap;
-
-
-
-/**
- * meters_per_pixel = 156543.03392 * Math.cos(latLng.lat() * Math.PI / 180) / Math.pow(2, zoom)
- */
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,33 +1,55 @@
-// https://www.geeksforgeeks.org/implementation-graph-javascript/
-
-// p5js stuff
-var gridWidth;
-var cellWidth;
-var bg;
-
-var grid;
-var pathGrid;
-var path = [];
-var initialized = false;
-var select = false;
-var enabled = true;
-var index;
-
 /**
- * Preload
+ * OKAY READ THIS
+ * 
+ * These two js files interact really oddly.
+ * They can sometimes talk to eachother via functions, but not access eachothers
+ * global vars. Because that totally makes sense. Fuck JS
+ * 
+ * The general rule of thumb I, Ethan Rudy, have followed is, this file manages
+ * HTML events, the map API, and any non-grid interactivity
+ * sketch.js on the otherhand handles all canvas events & visuals, pathing, etc.
+ */
+
+
+// p5js stuff, dont mix it with index.js
+
+var gridDimension;      // Grid dimension (its square)
+var cellDimensionPix;   // Cell dimensions in pixels
+var bg;                 // Background Image
+
+var grid;                 // Selection Grid
+var pathGrid;             // Path Grid (modified Selection grid)
+var path = [];            // Path array
+var initialized = false;  // Flag for if the index.js grid has been initialized
+var clicked = false;      // Mouse clicked flag
+var enabled = true;       // Cell Enable/Disable flag
+/**
+ * Preload function
+ * 
+ * Calls when the page is loading, BEFORE THE CANVAS IS CREATED
  */
 function preload() {
   bg = loadImage("./images/default.png");
 }
 
 /**
- * Runs once on page load
+ * Runs once on page full
  */
 function setup() {
-    createCanvas(640, 640);
-    fill(0, 0, 0, 0);
-    strokeWeight(1);
-    stroke(0, 0, 0);
+
+  // Canvas of max size of the free google static maps API 
+  createCanvas(640, 640);
+
+  // Set our fill color to black
+  fill(0, 0, 0, 0);
+
+  // Set line width to 1
+  strokeWeight(1);
+
+  // Set line color to black
+  stroke(0, 0, 0);
+
+  // Okay, I hope you see the pattern of rgb, cuz Im not gonna comment all the changes I make
 }
 
 /**
@@ -39,11 +61,18 @@ function draw() {
   // Set the background iamge
   background(bg);
 
+  // If the grid has been initialized
   if (initialized) {
+    // Update the grid
+    // This polls events and detects mouse input
     updateGrid();
 
+    // Draw the grid to the canvas
     drawGrid();
     
+    // TEMPORARY
+    // Draw path
+    // Does some color stuff too!
     let amt = 255.0 / path.length;
     let c = 0;
     if (path.length != 0) {
@@ -55,10 +84,10 @@ function draw() {
         stroke(c);
 
         line(
-          first[0] * cellWidth + cellWidth / 2, 
-          first[1] * cellWidth + cellWidth / 2,
-          second[0] * cellWidth + cellWidth / 2, 
-          second[1] * cellWidth + cellWidth / 2
+          first[0] * cellDimensionPix + cellDimensionPix / 2, 
+          first[1] * cellDimensionPix + cellDimensionPix / 2,
+          second[0] * cellDimensionPix + cellDimensionPix / 2, 
+          second[1] * cellDimensionPix + cellDimensionPix / 2
         );
         c += amt;
 
@@ -70,30 +99,36 @@ function draw() {
 
 // Enable mouse input
 function mousePressed() {
-  select = true;
+  clicked = true;
 }
 
 // Disable mouse input
 function mouseReleased() {
-  select = false;
+  clicked = false;
 }
 
 /**
  * Update Grid
  * 
- * Updates the grid with the mouse input
+ * Updates the grid using the onclick event back in index.js on the HTML button
+ * This is an example of index.js being able to access sketch.js functions
  */
 function updateGrid() {
-  var xPos = Math.floor(mouseX / cellWidth);
-  var yPos = Math.floor(mouseY / cellWidth);
+  // Convert mouse coordinates to grid coordinates
+  var xPos = Math.floor(mouseX / cellDimensionPix);
+  var yPos = Math.floor(mouseY / cellDimensionPix);
 
-  if (xPos >= 0 && xPos < gridWidth && yPos >= 0 && yPos < gridWidth && select) {
+  // Check bounds
+  if (xPos >= 0 && xPos < gridDimension && yPos >= 0 && yPos < gridDimension && clicked) {
+    // Enable mode
     if (enabled) {
-      grid[yPos][xPos] = true;  
-      pathGrid[yPos][xPos] = 0;
+      grid[yPos][xPos] = true;    // Enabled
+      pathGrid[yPos][xPos] = 0;   // Not visited
+
+    // Disable mode
     } else {
-      grid[yPos][xPos] = false;  
-      pathGrid[yPos][xPos] = -1;
+      grid[yPos][xPos] = false;   // Disabled
+      pathGrid[yPos][xPos] = -1;  // DONT GO HERE, obv its not in the boundary
     }
     
   }
@@ -108,9 +143,10 @@ function updateGrid() {
  * 
  */
 function updateGridData(a, b) {
-  gridWidth = a;
-  cellWidth = b;
+  gridDimension = a;
+  cellDimensionPix = b;
 
+  // Reset/Initialize all the grids
   initGrid();
 }
 
@@ -118,7 +154,6 @@ function updateGridData(a, b) {
  * Update Background
  * 
  * @param bg_link Link to the background
- * 
  */
 function updateBackground(bg_link) {
     bg = loadImage(bg_link);
@@ -142,12 +177,20 @@ function setDisabled() {
   enabled = false;
 }
 
+/**
+ * Generate Path
+ * 
+ * TEMPRARY PART: Starting point is precomputed to be the most NW point, to make my life easier, do change this
+ * 
+ * Generates the path after finding the TEMPORARY starting point
+ */
 function generatePath() {
   // Find the starting point
+  // BEGIN TEMPORARY
   found = false;
   var point_x, point_y;
-  for (let y = 0; y < gridWidth && !found; ++y) {
-    for (let x = 0; x < gridWidth && !found; ++x) {
+  for (let y = 0; y < gridDimension && !found; ++y) {
+    for (let x = 0; x < gridDimension && !found; ++x) {
       if (pathGrid[y][x] == 0) {
         // First non-visited, enabled cell
         found = true;
@@ -155,91 +198,103 @@ function generatePath() {
       }
     }
   }
+  // END TEMPORARY
 
-  path = [];
-  index = 0;
+  path = [];  // Reset / Init path list
 
-  console.log(pathGrid);
-
+  // Copy the untouched pathGrid over to be replaced later
+  // I put this here incase they regenerate the path without changing the grid size
+  // Which would just append the new path to the old one and run on a modified pathGrid
+  // Hence the copy and reset
   let oldGrid = [];
-
-  for (let y = 0; y < gridWidth; ++y) {
+  for (let y = 0; y < gridDimension; ++y) {
     oldGrid[y] = [];
-    for (let x = 0; x < gridWidth; ++x) {
+    for (let x = 0; x < gridDimension; ++x) {
       oldGrid[y][x] = pathGrid[y][x];
     }
   }
 
-  //print(point_x + ", " + point_y);
+  // Find the path
   findPath(point_x, point_y);
 
-  for (let y = 0; y < gridWidth; ++y) {
+  // Reset the pathGrid
+  for (let y = 0; y < gridDimension; ++y) {
     pathGrid[y] = [];
-    for (let x = 0; x < gridWidth; ++x) {
+    for (let x = 0; x < gridDimension; ++x) {
       pathGrid[y][x] = oldGrid[y][x];
     }
   }
 
+  // Print path length :(
   console.log("Path Length: " + path.length);
-  pathString = "";
-  for (let i = 0; i < path.length && path.length != 0; ++i) {
-    pathString += "(" + path[i][0] + ", " + path[i][1] + ")\n";
-  }
-  console.log(pathString);
 }
 
-
-
 /**
- * This is really shit code, but its like 1 am after an 8 hour shift
+ * Find Path (recursive)
+ * 
+ * Simultaneously the best and worst function I've ever written
  */
 function findPath(x, y) {
-  // If the grid is complted, stop calling
+  // If the grid is completely visited, stop calling
   if (checkGrid()) { return; }
 
-  print(x + ", " + y + "\n");
-
+  // DEAD END CODE
+  // Path is long enough to hit a dead end
   if (path.length > 1) {
+    // .back() index like std::vector<>.back() from c++, this code is ported so I didn't have to debug in mozilla
     let back = path.length - 1
+    // Distance between the last grid cell visited and where we are trying to go
     let dist = abs(path[back][0] - x) + abs(path[back][1] - y);
+    // Index of the cell to backtrack through
     let toCopy = back;
+
+    // While the jump is impossible
     while (dist > 1) {
         --toCopy;
         path.push(path[toCopy]);
+        // Backtrack a cell
+
         ++back;
         dist = abs(path[back][0] - x) + abs(path[back][1] - y);
+        // Calculate the new distance
     }
   }
 
 
+  // Append where we are
   path.push([x, y]);
-
+  // Mark where we are as visited
   pathGrid[y][x] = 1;
 
-  // Right Path
-  if (x < gridWidth && pathGrid[y][x + 1] == 0) {
+  // Right Path is in bounds and not-visited
+  if (x < gridDimension && pathGrid[y][x + 1] == 0) {
       findPath(x + 1, y);
   }
 
-  // Left Path
+  // Left Path is in bounds and not-visited
   if (x > 0 && pathGrid[y][x - 1] == 0) {
       findPath(x - 1, y);
   }
 
-  // Down Path
-  if (y < gridWidth && pathGrid[y + 1][x] == 0) {
+  // Down Path is in bounds and not-visited
+  if (y < gridDimension && pathGrid[y + 1][x] == 0) {
       findPath(x, y + 1);
   }
 
-  // Up Path
+  // Up Path is in bounds and not-visited
   if (y > 0 && pathGrid[y - 1][x] == 0) {
       findPath(x, y - 1);
   }
 }
 
+/**
+ * Check Grid
+ * 
+ * @returns If the grid is completed
+ */
 function checkGrid() {
-  for (let y = 0; y < gridWidth; ++y) {
-    for (let x = 0; x < gridWidth; ++x) {
+  for (let y = 0; y < gridDimension; ++y) {
+    for (let x = 0; x < gridDimension; ++x) {
       if (pathGrid[y][x] == 0) { return false; }
     }
   }
@@ -249,16 +304,18 @@ function checkGrid() {
 /**
  * Initialize Grid
  * 
- * Initializes a square, fully disabed grid
+ * Initializes a square, fully disabled grid, pathGrid
+ * Resets path
+ * Flags the grid as initialized
  */
 function initGrid() {
   grid = [];
   pathGrid = [];
   path = [];
-  for (let y = 0; y < gridWidth; ++y) {
+  for (let y = 0; y < gridDimension; ++y) {
     grid[y] = [];
     pathGrid[y] = [];
-    for (let x = 0; x < gridWidth; ++x) {
+    for (let x = 0; x < gridDimension; ++x) {
       grid[y][x] = 0;
       pathGrid[y][x] = -1;
     }
@@ -275,14 +332,14 @@ function drawGrid() {
 
   strokeWeight(1);
 
-  for (let y = 0; y < gridWidth; ++y) {
-    for (let x = 0; x < gridWidth; ++x) {
+  for (let y = 0; y < gridDimension; ++y) {
+    for (let x = 0; x < gridDimension; ++x) {
       if (grid[y][x] == 0){
         noFill();
       } else {
         fill(255, 0, 0, 100);
       }
-      rect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+      rect(x * cellDimensionPix, y * cellDimensionPix, cellDimensionPix, cellDimensionPix);
     }
   }
 }
